2092e10e7c44d94cb4c89ba3eb828c30f5da9c0c
baptwr <baptiste.hennecart@ird.fr>
2026-03-19 10:34:56 +0100

    Updated Docker image version to v0.2.0-pre1 in build script

--- .docker/build.sh
+++ .docker/build.sh
@@ -2,7 +2,7 @@
 # build and push strainmake Docker image
 
 IMAGE=strainmake
-VERSION=v0.1.0-pre2
+VERSION=v0.2.0-pre1
 
 docker build -t bapt931894/${IMAGE}:latest -t bapt931894/${IMAGE}:${VERSION} .
 docker push bapt931894/${IMAGE}:latest
