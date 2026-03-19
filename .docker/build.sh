ce6e5980c81ca22746f490c2e148c54bc38d797b
baptwr <baptiste.hennecart@ird.fr>
2026-03-19 15:31:37 +0100

    Added --no-cache option to Docker build

--- .docker/build.sh
+++ .docker/build.sh
@@ -4,7 +4,7 @@
 IMAGE=strainmake
 VERSION=v0.2.0-pre1
 
-docker build -t bapt931894/${IMAGE}:latest -t bapt931894/${IMAGE}:${VERSION} .
+docker build --no-cache -t bapt931894/${IMAGE}:latest -t bapt931894/${IMAGE}:${VERSION} .
 
 # verifying the image starts, reports a version, and lists commands
 echo "==> Smoke-testing image..."
